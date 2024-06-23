1b26083dcf3f906185d296332990f7734d82dbfb
Yetiuard <138736990+Yetiuard@users.noreply.github.com>
2024-06-24 02:49:26 +0300

    add blog

---
+++ yetiiverse/assets/user/js/post_index.js
@@ -0,0 +1,3 @@
+var post_index = [
+    { file_name: "./assets/user/js/posts/2024.js", var_name: "posts2024" },
+];
---
+++ yetiiverse/assets/user/js/posts/2024.js
@@ -0,0 +1,16 @@
+var posts2024 = [
+    {
+        is_played: 1,
+        topic_tag: "Dragon Roost Island",
+        subtype_class: "post-subtype-label-via-api",
+        mii: "frustrated",
+        body: "how do i make it color?",
+        community: "14866558073070242127",
+        created_at: "02/01/2017 15:59",
+        subtype: "In-Game",
+        ss: 1,
+        id: "AYMHAAADAAB2V0flVHiyOA",
+        reply_count: 3,
+        empathy_count: "2",
+    },
+];
