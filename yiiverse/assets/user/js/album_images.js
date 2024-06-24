8648319abc05d87ba1cba67d76090ee190c31a5e
Yetiuard <138736990+Yetiuard@users.noreply.github.com>
2024-06-25 00:26:43 +0300

    erk

--- yiiverse/assets/user/js/album_images.js
+++ yiiverse/assets/user/js/album_images.js
@@ -1,2 +1,2 @@
-//var albumImages =
-//[{"album":[{"created_at":"Saved on 26/06/2016 21:58","ss":"test"}],"name":"Looking Around","id":"14866558073568523354"}]
+var albumImages =
+[{"album":[{"created_at":"Saved on 26/06/2016 21:58","ss":"test"}],"name":"Looking Around","id":"14866558073568523354"}]
