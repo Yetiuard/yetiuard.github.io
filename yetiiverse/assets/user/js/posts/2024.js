de4a954755d7c9a6367414b20b9db32fec3de3ef
Yetiuard <138736990+Yetiuard@users.noreply.github.com>
2024-06-24 03:26:31 +0300

    q

--- yetiiverse/assets/user/js/posts/2024.js
+++ yetiiverse/assets/user/js/posts/2024.js
@@ -1,11 +1,11 @@
 var posts2024 = [
     {
         is_played: 1,
-        topic_tag: "Dragon Roost Island",
+        topic_tag: "",
         subtype_class: "post-subtype-label-via-api",
-        mii: "frustrated",
-        body: "how do i make it color?",
-        community: "14866558073070242127",
+        mii: "normal",
+        body: "Work In Progress",
+        community: "",
         created_at: "02/01/2017 15:59",
         subtype: "In-Game",
         ss: 1,
