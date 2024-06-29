c370a0e14cf62d84ec04d4f1462790af5db5d2c7
Yetiuard <138736990+Yetiuard@users.noreply.github.com>
2024-06-29 23:29:32 +0300

    add first image batch

--- yiiverse/assets/user/js/album_images.js
+++ yiiverse/assets/user/js/album_images.js
@@ -1,2 +1,15 @@
-//var albumImages =
-//[{"album":[{"created_at":"Saved on 26/06/2016 21:58","ss":"test"}],"name":"Looking Around","id":"14866558073568523354"}]
+var albumImages = [
+    {
+        album: [
+            { created_at: "Uploaded at 6/29/24 23:21. Seems to be a Dacia 1300.", ss: "3ds/OldCar" },
+            { created_at: "Uploaded at 6/29/24 23:21. Tree!", ss: "3ds/Tree" },
+            { created_at: "Uploaded at 6/29/24 23:21. Some nice grafitti mural.", ss: "3ds/Mural" },
+            { created_at: "Uploaded at 6/29/24 23:21. Badass car ngl", ss: "3ds/Hummer" },
+            { created_at: "Uploaded at 6/29/24 23:21. Some grafitti", ss: "3ds/Grafitti2" },
+            { created_at: "Uploaded at 6/29/24 23:21. Some grafitti", ss: "3ds/Grafitti1" },
+            { created_at: "Uploaded at 6/29/24 23:21. Girl with pearl earing", ss: "3ds/GirlWithPearlEarring" },
+        ],
+        name: "3DS Photography",
+        id: "14866558073568523354",
+    },
+];
